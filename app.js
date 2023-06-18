d040bf7e333c1f4dd00234d861a2e1499252c84d
Ge Xu <gexu13@outlook.com>
2023-06-18 13:21:16 -0700

    configure mongodb

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const express = require('express')
+import express from 'express';
+import cors from 'cors';
+import { mongo } from 'mongoose';
+
+mongo.connect('mongodb+srv://web-final:0YPyPauA8yYbcuyW@cluster0.j6ysphf.mongodb.net/?retryWrites=true&w=majority');
+
 const app = express()
+
+app.use(
+    cors({
+        credentials: true,
+        origin: 'http://localhost:3000'
+    })
+);
+
+app.use(
+    session({
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+
+app.use(express.json());
+
+
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 app.listen(4000)
---
+++ controllers/users-dao.js
@@ -0,0 +1,34 @@
+
+export const findAllUsers = () => {
+    return usersModel.find();
+};
+
+
+export const findUserById = (uid) => {
+    return usersModel.findById(uid);
+};
+
+
+export const findUserByUsername = (username) => {
+    return usersModel.findOne({username: username});
+};
+
+
+export const findUserByCredentials = (username, password) => {
+    return usersModel.findOne({username: username, password: password});
+};
+
+
+export const createUser = (user) => {
+    return usersModel.create(user);
+}
+
+
+export const updateUser = (id, user) => {
+    return usersModel.updateOne({_id: id}, {$set: user});
+};
+
+
+export const deleteUser = (uid) => {
+    return usersModel.deleteOne({_id: uid});
+};
